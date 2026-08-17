37931891a8a52dd521e4e27d2a9f4b653f61853d
Joykan <joykandie07@gmail.com>
2026-08-17 22:31:17 +0300

    fix: add geminiApiKey to Vercel secrets.dart generation

--- vercel_build.sh
+++ vercel_build.sh
@@ -8,6 +8,7 @@ cat <<EOF > lib/core/constants/secrets.dart
 class Secrets {
   static const supabaseUrl = '${SUPABASE_URL}';
   static const supabaseAnonKey = '${SUPABASE_ANON_KEY}';
+  static const geminiApiKey = '${GEMINI_API_KEY}';
 }
 EOF
 
